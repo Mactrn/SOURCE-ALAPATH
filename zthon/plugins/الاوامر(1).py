9fb96ece15610c95d3003763bfcd3a4375f26fb9
Mactrn <123252080+Mactrn@users.noreply.github.com>
2023-02-19 13:42:19 +0300

    Update الاوامر(1).py

--- zthon/plugins/الاوامر(1).py
+++ zthon/plugins/الاوامر(1).py
@@ -373,3 +373,40 @@ async def repozedub(event):
     await response[0].click(event.chat_id)
     await event.delete()
 
+    @jepiq.ar_cmd(
+    pattern="المطور$",
+    command=("المطور", plugin_category),
+    info={
+        "header": "لأظهار مطورين السورس",
+        "usage": [
+            "{tr}المطور",
+        ],
+    },
+)
+async def amireallyalive(event):
+    "A kind of showing bot details"
+    reply_to_id = await reply_id(event)
+    uptime = await get_readable_time((time.time() - StartTime))
+    _, check_sgnirts = check_data_base_heal_th()
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
+    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
+    CAT_IMG = " https://telegra.ph/file/83d3c9610bc957b20e35a.jpg "
+    if CAT_IMG:
+        CAT = [x for x in CAT_IMG.split()]
+        A_IMG = list(CAT)
+        PIC = random.choice(A_IMG)
+        cat_caption = f"مطور لعابث\n"
+        cat_caption += f"✛━━━━━━━━━━━━━✛\n"
+        cat_caption += f"- المطور  : @ALAPATH\n"
+        cat_caption += f"- المطور  : @ALAPATH\n"
+        cat_caption += f"✛━━━━━━━━━━━━━✛\n"
+        await event.client.send_file(
+            event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
+        )
+
+@jepiq.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
+async def on_plug_in_callback_query_handler(event):
+    statstext = await catalive(StartTime)
+    await event.answer(statstext, cache_time=0, alert=True)
+
+    
